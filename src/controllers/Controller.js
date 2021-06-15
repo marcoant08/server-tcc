001a26c1499dae32fc0bb85b8a8a7124ba810276
marcoant08 <marcoant008@gmail.com>
2021-06-15 20:31:31 -0300

    bugou foi tudo aff

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,9 +1,22 @@
-require("dotenv").config();
+// require("dotenv").config();
+// // const faceapi = require("../lib/face-api/face-api.min.js");
+// // global.fetch = require('node-fetch');
 
-module.exports = {
-  async index(req, res) {
-    console.log(process.env.TESTE);
+// // Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri("../lib/face-api/models")]);
 
-    return res.send();
-  },
-};
+// module.exports = {
+//   async index(req, res) {
+//     console.log(process.env.TESTE);
+
+//     return res.send();
+//   },
+// };
+'use strict';
+
+const request = require('request');
+
+require('dotenv').config();
+
+const subscriptionKey = process.env.SUBSCRIPTION_FACE_API_KEY;
+const uriBase = process.env.URI_BASE;
+const imageUrl = 'https://cdn-ofuxico.akamaized.net/img/upload/noticias/2019/05/13/silvio_santos_reproducao_instagram_349201_36.jpg';
