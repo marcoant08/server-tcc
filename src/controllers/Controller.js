5e3245d941a571e12e909793c4543cb828b2c462
unknown <marco.goncalves@mrpay.com.br>
2021-06-22 09:13:56 -0300

    segunda chance

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,22 +1,11 @@
-// require("dotenv").config();
-// // const faceapi = require("../lib/face-api/face-api.min.js");
-// // global.fetch = require('node-fetch');
+"use strict";
 
-// // Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri("../lib/face-api/models")]);
+require("dotenv").config();
 
-// module.exports = {
-//   async index(req, res) {
-//     console.log(process.env.TESTE);
+module.exports = {
+  async index(req, res) {
+    console.log(process.env.TESTE);
 
-//     return res.send();
-//   },
-// };
-'use strict';
-
-const request = require('request');
-
-require('dotenv').config();
-
-const subscriptionKey = process.env.SUBSCRIPTION_FACE_API_KEY;
-const uriBase = process.env.URI_BASE;
-const imageUrl = 'https://cdn-ofuxico.akamaized.net/img/upload/noticias/2019/05/13/silvio_santos_reproducao_instagram_349201_36.jpg';
+    return res.send(process.env.TESTE);
+  },
+};
